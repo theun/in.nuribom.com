444983d6d396b6ff5cc15aabad983f8714f12c8f
Taehwan Eun <theun0524@gmail.com>
2012-05-17 21:17:24 +0900

    gunicorn execute with development.ini to production.ini

--- script.sh
+++ script.sh
@@ -4,4 +4,4 @@ LOGFILE=/var/log/gunicorn/nurin.log
 LOGDIR=$(dirname $LOGFILE)
 source ../env/bin/activate
 test -d $LOGDIR || mkdir -p $LOGDIR
-exec ../env/bin/gunicorn_paster -c gunicorn.conf.py development.ini
+exec ../env/bin/gunicorn_paster -c gunicorn.conf.py production.ini
